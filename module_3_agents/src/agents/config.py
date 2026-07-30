bbadbe151ac1098d1fc422b71eda473665ae36cd
Matvey Ilienkov <ahalaah0@gmail.com>
2026-07-30 17:39:53 +0800

    fix: fix config.py conflicts

--- module_3_agents/src/agents/config.py
+++ module_3_agents/src/agents/config.py
@@ -3,19 +3,11 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-<<<<<<< HEAD
-
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 if not GOOGLE_API_KEY:
-    raise ValueError("КРИТИЧЕСКАЯ ОШИБКА: GOOGLE_API_KEY не найден. Проверь файл .env")
+    raise ValueError("КРИТИЧЕСКАЯ ОШИБКА: GOOGLE_API_KEY не найден. Проверьте файл .env")
 
 FASTAPI_ENDPOINT = "http://localhost:8000/search"
 
 LLM_MODEL = "gemini-3.5-flash-lite"
-=======
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-
-if not GOOGLE_API_KEY:
-    raise ValueError("GOOGLE_API_KEY не найден. Проверьте файл .env")
->>>>>>> f68b5f5529685c707b09ec080e7ebecb83d9d5e2
